240c34aa9fba9212d1db2eb818fc8457dc3f603f
Ilya Dutlov <id@dutlov.ru>
2021-11-26 00:12:39 +0300

    add welcome func with prompt to enter username and print hello

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
+import prompt
+from brain_games.cli import welcome_user
+
 def greet():
     print('Welcome to the Brain Games!')
 
 def main():
 	greet()
+	welcome_user()
+	
 
 if __name__ == '__main__':
     main()
