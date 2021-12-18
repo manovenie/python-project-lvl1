a52a0bf306ca03fa5615dae13bab5c278d7dccf2
Ilya Dutlov <id@dutlov.ru>
2021-12-19 02:20:24 +0300

    refactor brain_even

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-from brain_games.cli import get_user_answer
 from brain_games.scripts.brain_games import welcome_user
+from brain_games.scripts.brain_games import print_game_instruction
+from brain_games.scripts.brain_games import game_process
 from brain_games.scripts.brain_games import generate_int
-from brain_games.scripts.brain_games import print_lose_message
 from brain_games.scripts.brain_games import COUNT_WINS_NEEDED
-import prompt
 
 
 GAME_NAME = 'brain-even'
@@ -19,24 +18,17 @@ def find_true_answer(generated_int):
         return 'no'
 
 
+def print_question_return_answer():
+    random_int = generate_int()
+    print('Question: {}'.format(random_int))
+    correct_answer = find_true_answer(random_int)
+    return correct_answer
+
+
 def main():
-    user_name = welcome_user(GAME_NAME)
-    counter_correct_answers = 0
-    while True:
-        random_int = generate_int()
-        print("Question: {}".format(random_int))
-        correct_answer = find_true_answer(random_int)
-        user_answer = get_user_answer(GAME_NAME)
-        if correct_answer == user_answer:
-            counter_correct_answers += 1
-            if counter_correct_answers < COUNT_WINS_NEEDED:
-                print('Correct!')
-                continue
-            else:
-                print("Congratulations, {}!".format(user_name))
-                break
-        else:
-            print_lose_message()
+    user_name = welcome_user()
+    print_game_instruction(GAME_NAME)
+    game_process(user_name)
 
 
 if __name__ == '__main__':
