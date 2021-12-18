393712e982a4bc71a0576a7be65d1ccb60259a20
Ilya Dutlov <id@dutlov.ru>
2021-12-19 02:23:20 +0300

    refactor brain_gcd

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-from brain_games.cli import get_user_answer
 from brain_games.scripts.brain_games import welcome_user
+from brain_games.scripts.brain_games import print_game_instruction
+from brain_games.scripts.brain_games import game_process
 from brain_games.scripts.brain_games import generate_int
-from brain_games.scripts.brain_games import print_lose_message
 from brain_games.scripts.brain_games import COUNT_WINS_NEEDED
-import prompt
 
 
 GAME_NAME = 'brain-gcd'
+
 
 def find_true_answer(int1, int2):
     biggest_divider = 1
@@ -19,24 +19,17 @@ def find_true_answer(int1, int2):
     return biggest_divider
 
 
+def print_question_return_answer():
+    random_int1, random_int2 = generate_int(), generate_int()
+    print('Question: {} {}'.format(random_int1, random_int2))
+    correct_answer = find_true_answer(random_int1, random_int2)
+    return correct_answer
+
+
 def main():
-    user_name = welcome_user(GAME_NAME)
-    counter_correct_answers = 0
-    while True:
-        random_int1, random_int2  = generate_int(), generate_int()
-        print('Question: {} {}'.format(random_int1, random_int2))
-        correct_answer = find_true_answer(random_int1, random_int2)
-        user_answer = get_user_answer(GAME_NAME)
-        if correct_answer == user_answer:
-            counter_correct_answers += 1
-            if counter_correct_answers < COUNT_WINS_NEEDED:
-                print('Correct!')
-                continue
-            else:
-                print("Congratulations, {}!".format(user_name))
-                break
-        else:
-            print_lose_message()
+    user_name = welcome_user()
+    print_game_instruction(GAME_NAME)
+    game_process(user_name)
 
 
 if __name__ == '__main__':
