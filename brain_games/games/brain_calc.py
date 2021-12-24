f7287eef93ab6874e19e7df8fb60a51c39963ad5
Ilya Dutlov <id@dutlov.ru>
2021-12-24 20:17:02 +0300

    del rebundant func find_answer in brain-calc and rewrite general func

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -19,9 +19,5 @@ def get_question_and_answer():
     random_int2 = random.randint(MIN_RANDOM_INT, MAX_RANDOM_INT)
     operation = random.choice(list(operations.keys()))
     question = '{} {} {}'.format(random_int1, operation, random_int2)
-    answer = find_true_answer(random_int1, random_int2, operation)
+    answer = str(operations[operation](int1, int2))
     return question, answer
-
-
-def find_true_answer(int1, int2, operation):
-    return str(operations[operation](int1, int2))
