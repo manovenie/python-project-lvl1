5a7f21756870c6b035858f8047987eda28d2ffef
Ilya Dutlov <id@dutlov.ru>
2021-12-24 22:43:56 +0300

    refactor find_gcd func with the help if euclidean algo

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -12,14 +12,15 @@ def get_question_and_answer():
     random_int1 = random.randint(MIN_RANDOM_INT, MAX_RANDOM_INT)
     random_int2 = random.randint(MIN_RANDOM_INT, MAX_RANDOM_INT)
     question = '{} {}'.format(random_int1, random_int2)
-    answer = find_true_answer(random_int1, random_int2)
+    answer = find_gcd(random_int1, random_int2)
     return question, answer
 
 
-def find_true_answer(int1, int2):
-    biggest_divider = 1
-    smallest_nbr = min(int1, int2)
-    for counter in range(biggest_divider, smallest_nbr + 1):
-        if int1 % counter == 0 and int2 % counter == 0:
-            biggest_divider = counter
-    return str(biggest_divider)
+def find_gcd(a, b):
+    while True:
+        if a == b:
+            return str(a)
+        elif a > b:
+            a = a - b
+        elif b > a:
+            b = b - a
