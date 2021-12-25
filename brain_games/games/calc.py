ed04d51ef1328beb85bca2006dddcc63d0ad52d0
Ilya Dutlov <id@dutlov.ru>
2021-12-26 00:42:26 +0300

    del turning dict to list in calc module in OPERATIONS, create list at the beginning

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,13 +6,13 @@ import random
 GAME_INSTRUCTION = 'What is the result of the expression?'
 MIN_RANDOM_INT = 1
 MAX_RANDOM_INT = 30
-OPERATIONS = {'-': sub, '+': add, '*': mul}
+OPERATIONS = [['-', sub], ['+', add], ['*', mul]]
 
 
 def get_question_and_answer():
     int1 = random.randint(MIN_RANDOM_INT, MAX_RANDOM_INT)
     int2 = random.randint(MIN_RANDOM_INT, MAX_RANDOM_INT)
-    symbol, operation = random.choice(list(OPERATIONS.items()))
+    symbol, operation = random.choice(OPERATIONS)
     answer = str(operation(int1, int2))
     question = '{} {} {}'.format(int1, symbol, int2)
     return question, answer
