15ed8b0da69a2fce4d4a62a4f15bc09c5896b73d
Ilya Dutlov <id@dutlov.ru>
2021-12-27 18:56:42 +0300

    fix brain-games script

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,12 +2,14 @@
 
 '''Welcome part of the game'''
 
-from brain_games.game_process import game_loop
+from brain_games.cli import get_user_name
 
 
 def main():
     """No game: just prints welcome messages"""
-    game_loop()
+    print('Welcome to the Brain Games!')
+    user_name = get_user_name()
+    print('Hello, {}!'.format(user_name))
 
 
 if __name__ == '__main__':
